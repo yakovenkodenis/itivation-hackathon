42909ae73b564aa7c63e81d9b2ff3912e7466327
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-08-02 12:48:42 +0300

    Generate devise_invitable views

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,5 +2,6 @@ class Mentor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
 end
--- app/models/teammate.rb
+++ app/models/teammate.rb
@@ -2,6 +2,7 @@ class Teammate < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable
   belongs_to :team
 end
