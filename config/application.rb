5f2c7fd2494068402f8472bcaac31a58d0b1711b
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-08-09 16:12:08 +0300

    serve assets

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module Hackathon
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.serve_static_assets = true
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_mailer.raise_delivery_errors = true
